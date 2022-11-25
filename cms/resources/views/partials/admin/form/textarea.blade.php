9ee340b45016b8eb9ed111ae1d71321846ef9260
robertera <robertokorzun@hotmail.com>
2022-11-25 17:49:39 -0300

    File manager funcionando

--- cms/resources/views/partials/admin/form/textarea.blade.php
+++ cms/resources/views/partials/admin/form/textarea.blade.php
@@ -1,6 +1,7 @@
 <div class="field">
     <label class="label">{{ __('admin.fields.' . $resource . '.' . $attribute) }}</label>
     <div class="control">
+        <title> ESTOU AQUI </title>
         <textarea id="editor" name="{{ $attribute }}" class="textarea" rows="{{ $rows ?? 10 }}">{{ ${$resource}->$attribute ?? old($attribute) }}</textarea>
     </div>
 </div>
